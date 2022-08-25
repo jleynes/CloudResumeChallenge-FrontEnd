4b2ae1360be609253d63ef292cf0042a5fe8eb69
jleynes <jleynes@asu.edu>
2022-08-24 23:33:35 -0700

    Added javaSctipt counter

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //Javascript to call AWS API
+
+//Website counter
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+var resetButton = document.querySelector("#reset");
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+//Reset button (for testing)
+resetButton.addEventListener("click", () => {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+    counterContainer.innerHTML = visitCount;
+})
